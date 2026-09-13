fe06dabf962109b3cdd2607fdfdd8bbc59570dd8
Elias Paloheimo <eliaspaloheimo@yahoo.com>
2026-09-13 20:47:33 +0300

    Fix leap year condition in Tehtävä3.4.py

--- Moduuli.3/Tehtävä3.4.py
+++ Moduuli.3/Tehtävä3.4.py
@@ -1,5 +1,5 @@
 Vuosiluku = int(input("Anna vuosiluku"))
-if Vuosiluku % 4 == 0 and Vuosiluku % 100 == 0 and Vuosiluku % 400 == 0:
+if Vuosiluku % 4 == 0 and Vuosiluku % 100 != 0 and Vuosiluku % 400 == 0:
     print("Vuosi on karkausvuosi,")
 else:
     print("Vuosi ei ole karkausvuosi.")
